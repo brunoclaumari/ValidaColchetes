25b67d443c6cadc2c50a63a8ee6fb9f5fc01627d
brunoclaumari <brunoclaumari@gmail.com>
2024-09-01 12:32:29 -0300

    Subindo proj console valida colchetes, com exemplos

---
+++ ValidaColchetes/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidaColchetes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            // Sequências válidas
+            string[] validas = {
+            "({[(){}]})",      // Válidas
+            "[({})({[]})]",    // Válidas
+            "{[()()][{}]}",    // Válidas
+            "[{([])}[{}]]",    // Válidas
+            "{[({})]()[]}",    // Válidas
+            "([{}][({})])",    // Válidas
+        };
+
+            // Sequências inválidas
+            string[] invalidas = {
+            "{[([)]}",          // Inválidas
+            "[{(})({[]})]",     // Inválidas
+            "({[})()({]})",     // Inválidas
+            "[{([])}{[}]}",     // Inválidas
+        };
+
+            // Exibindo as sequências válidas
+            int contador = 0;
+            foreach (var itemEntrada in validas)
+            {
+                contador++;
+                Console.WriteLine($"Resultado {contador}: {itemEntrada} Válido: {EntradaValida(itemEntrada)}");
+            }
+            
+            // Exibindo as sequências inválidas
+            foreach (var itemEntrada in invalidas)
+            {
+                contador++;
+                Console.WriteLine($"Resultado {contador}: {itemEntrada} Válido: {EntradaValida(itemEntrada)}");
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Esta é a função que determina se a sequencia de colchetes é válida (balanceada)
+        /// </summary>
+        /// <param name="entrada"></param>
+        /// <returns></returns>
+        static bool EntradaValida(string entrada)
+        {
+            List<char> listaAbertura = new List<char> { '(','[', '{' };
+            List<char> listaFechamento = new List<char> { ')', ']', '}' };
+            Dictionary<char, char> Verificador = new Dictionary<char, char>() 
+            {
+                {'(',')'},
+                {'[',']'},
+                {'{','}'}
+            };
+            bool retorno = false;
+            Stack<char> pilha = new Stack<char>();
+
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                char simboloAtual = entrada[i];
+                if (listaAbertura.Contains(simboloAtual))
+                {
+                    pilha.Push(simboloAtual);
+                }
+                else if(listaFechamento.Contains(simboloAtual))
+                {
+                    try
+                    {
+                        char ultimo = pilha.Peek();
+                        char fechamento = Verificador[ultimo];
+                        if (fechamento == simboloAtual)
+                        {
+                            pilha.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        //Se deu exception, é porque tentou desempilhar na pilha vazia;
+                        return false;                        
+                    }
+                }
+
+            }
+
+            retorno = pilha.Count == 0;
+            return retorno;
+        }
+    }
+}
